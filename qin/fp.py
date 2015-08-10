2f0da1ed4d73a27d3d7c655b1dd619e1295249b2
布鲁 <10461540@qq.com>
2015-08-10 11:30:17 +0800

    add fp.py

---
+++ qin/fp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+# author: b0lu
+# mail: b0lu_xyz@163.com
+from pipetools import *
+from pipetools import X as _
+from pipetools import pipe as P
+from pipetools import xpartial as XF
+from functools import partial as F
+from itertools import *
+
